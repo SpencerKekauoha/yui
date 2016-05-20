33f20c12917ec575827fa8f40ed7a166d02d778f
Spencer Kekauoha <spkekauoha@gmail.com>
2016-05-20 15:15:32 -0600

    done

--- server/public/js/directives/sloth/sloth.js
+++ server/public/js/directives/sloth/sloth.js
@@ -15,7 +15,32 @@ angular.module("yui").directive("changeCssDirective", function(){
         $('.yui-border-box-e').prepend('<img id="theImg" src="http://i.giphy.com/TniotlrhXtkZ2.gif" />');
         $('#bad-crying-text').html('Saved by his pincers of peril!');
         $('.yui-logo-hide-box').css("background", "url(http://i.giphy.com/3P3cTP0ToLS6c.gif");
-        $(".yui-view-container").css("background-color", "white");
+        $(".yui-view-container").css("background-color", "pink");
+        $(".animation-container").css("background-color", "pink");
+      });
+    }
+  };
+});
+
+angular.module("yui").directive("changeBack", function(){
+
+  return  {
+    restrict: "A",
+    link: function (scope, element, attribute) {
+
+      $('.directive-back-btn').on('click', function(){
+
+        $('#data').remove();
+        $('#bad-crying-1').attr("src", '/styles/images/yui-info-card.png');
+        $(".yui-info-box-text-sm").html("What is Yui?");
+        $(".yui-info-box").css("background", "url(./styles/images/smoke.jpeg");
+        $("#sloth-text").html("In Okinawan, Yui means to connect, collobrate, or come together as a group to acommplisha task.  Yui was created to connect designers with businesses that are in need of designs for their projects.");
+        $(".video").prepend();
+        $('.yui-border-box-e').prepend('<img id="theImg" src="http://i.giphy.com/TniotlrhXtkZ2.gif" />');
+        $('#bad-crying-text').html('Yui welcomes designers of all fields to publish their work.');
+        $('.yui-logo-hide-box').css("background", "url(http://i.giphy.com/3P3cTP0ToLS6c.gif");
+        $(".yui-view-container").css("background-color", "#000");
+        $(".animation-container").css("background-color", "#000");
       });
     }
   };
